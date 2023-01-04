204065e6bf17a547c984483640737c01a7adb473
Grace Joh <graceehjoh@gmail.com>
2023-01-04 14:59:00 -0600

    Make arrays

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+# Declare a variable that stores an Array of at least 4 Strings.
+arr_strings = ["this", "is", "really", "fun"]
+
+# Declare a variable that stores an Array of at least 4 Integers.
+arr_integers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+# Declare a variable that stores an Array of at least 4 Floats.
+arr_floats = [1.0, 1.25, 1.5, 1.75, 2.0]
+
+# Declare a variable that stores an Array of at least 4 Booleans.
+arr_bool = [true, true, false, false]
+
